a7852ac7d8e0db871601c3e802dcb18c41c38cdf
PriyeshGpta <priyeshgupta27111999@gmail.com>
2026-08-19 22:09:29 +0530

    feat(server.js): CRUD using express

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
-const http = require("http");
-const PORT = 3000;
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-    if (req.url === "/") res.end("Home endpoint");
-    if (req.url === "/products") res.end("Product endpoint");
-    if (req.url === "/users") res.end("Users endpoint");
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+let products = [];
+
+// GET Api
+app.get("/", (req, res) => {
+    res.send(products);
 })
 
-server.listen(PORT, () => {
+// POST Api
+app.post("/products", (req, res) => {
+    products.push(req.body);
+    res.send(req.body);
+})
+
+// PUT Api
+app.put("/product/:id", (req, res) => {
+    const { params, body } = req;
+    const { id } = params;
+
+    const updatedProducts = products.map((product) => product.id === id ? { id, ...body } : product);
+    products = updatedProducts
+
+    res.send(updatedProducts);
+})
+
+// DELETE Api
+app.delete("/product/:id", (req, res) => {
+    const { id } = req.params;
+
+    const updatedProducts = products.filter((product) => product.id !== id);
+    products = updatedProducts;
+
+    res.send(updatedProducts);
+})
+
+app.listen(PORT, () => {
     console.log(`Server is  running at PORT: ${PORT}`)
 })
